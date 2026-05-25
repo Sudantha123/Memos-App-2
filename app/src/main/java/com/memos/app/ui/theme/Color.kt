570c9cdfa0bd484380cdd135b7ec5eca4baf3c28
Sudantha123 <99850960+Sudantha123@users.noreply.github.com>
2026-05-26 01:03:45 +0530

    Create Color.kt

---
+++ app/src/main/java/com/memos/app/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.memos.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue40        = Color(0xFF1A73E8)
+val BlueGrey40    = Color(0xFF006495)
+val Green40       = Color(0xFF006E1C)
+
+val Blue80        = Color(0xFF9ECAFF)
+val BlueGrey80    = Color(0xFF72D1FF)
+
+val Blue10        = Color(0xFF001C38)
+val BlueGrey10    = Color(0xFF001E31)
