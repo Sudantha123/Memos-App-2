7dac1a03f31fee695a6507f0ed5cb1b2908222ee
Sudantha123 <99850960+Sudantha123@users.noreply.github.com>
2026-05-26 07:28:25 +0530

    Update AuthModels.kt

--- app/src/main/java/com/memos/app/data/api/models/AuthModels.kt
+++ app/src/main/java/com/memos/app/data/api/models/AuthModels.kt
@@ -3,12 +3,15 @@ package com.memos.app.data.api.models
 import com.google.gson.annotations.SerializedName
 
 data class SignInRequest(
-    @SerializedName("username") val username: String,
-    @SerializedName("password") val password: String
+    @SerializedName("username")    val username: String,
+    @SerializedName("password")    val password: String,
+    @SerializedName("neverExpire") val neverExpire: Boolean = false
 )
 
 data class AuthResponse(
     @SerializedName("id")          val id: Int    = 0,
     @SerializedName("username")    val username: String = "",
-    @SerializedName("displayName") val displayName: String = ""
+    @SerializedName("displayName") val displayName: String = "",
+    @SerializedName("accessToken") val accessToken: String = ""
 )
+
